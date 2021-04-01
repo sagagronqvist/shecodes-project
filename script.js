232ab243dd734754b51c387c63104ba2b9bab7a6
sagagronqvist <79092595+sagagronqvist@users.noreply.github.com>
2021-03-31 19:29:46 -1200

    New onecall api added to javascript

--- script.js
+++ script.js
@@ -25,18 +25,21 @@ switchDate();
 function switchCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#searched-input");
-  let cityInputForcast = document.querySelector("#searched-input");
   let apiKey = "3719f2615e58e78c75e87773957510b9";
 
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemp);
 
-  let apiForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInputForcast.value}&units=metric`;
-  axios.get(`${apiForecastUrl}&appid=${apiKey}`).then(displayForecast);
 }
 
   let form = document.querySelector("#search-form");
   form.addEventListener("submit", switchCity);
+
+  function getForecast(coordinates) {
+  let apiKey = "3719f2615e58e78c75e87773957510b9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+  }
 
 function displayTemp (response) {
 
@@ -60,7 +63,7 @@ function displayTemp (response) {
 
 // Forecast (Connected to geoCitySwitch and switchCity)
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
